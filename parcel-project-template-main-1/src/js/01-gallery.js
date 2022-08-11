c6afcfd1bd4da2633808a8282726e899b6b3c690
uliasuprun <uliasuprun88@gmail.com>
2022-08-11 19:05:43 +0300

    added start files

---
+++ parcel-project-template-main-1/src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryCardsMarkup(galleryItems);
+
+const simpleLightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+function createGalleryCardsMarkup(array) {
+    return array
+    .map(item => {
+        return `
+        <a class="gallery__item" href="${item.original}">
+            <img 
+            class="gallery__image" 
+            src="${item.preview}" 
+            alt="${item.description}" />
+        </a>`
+    })
+    .join('');
+}
