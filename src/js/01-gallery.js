bffcb232b06047ba8ff8b884461466a1b5c978dc
uliasuprun <uliasuprun88@gmail.com>
2022-08-11 19:58:11 +0300

    task 01 is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,7 @@
+// Описан в документации
+    import SimpleLightbox from "simplelightbox";
+    // Дополнительный импорт стилей
+    import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
@@ -6,7 +10,7 @@ console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 galleryContainer.innerHTML = createGalleryCardsMarkup(galleryItems);
 
-const simpleLightbox = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
 });
